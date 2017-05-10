16bf0141bbb2100869f143537e73d98d917af439
ceasarantonio <ceasarantonio@users.noreply.github.com>
2017-05-10 13:10:13 +0200

    done

--- js/Js.jsx
+++ js/Js.jsx
@@ -1,42 +1,87 @@
 
-class InputText extends React.Component{
-  render () {
-  let x;
-  return (<p>{this.props.change} {x}</p>);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+   }
+
+  handleChange(props) {
+    this.setState({value: props.target.value});
+  }
+
+  
+
+  render() {
+    return (
+      <div>
+         <input placeholder="Write something" type="text" value={this.state.value} onChange={this.handleChange} />
+            <p>{this.state.value}</p>
+      </div>
+    );
   }
 }
 
-let input = document.getElementById('input');
 
-function InputChanged(event){
-  ReactDOM.render(
-    <InputText change = {input.value} />,
-    document.getElementById('output')
-  )
-}
-input.addEventListener('keyup', InputChanged);
 
-class Sum extends React.Component{
-  render () {
-    let x = Number(number.value) + Number(number2.value);
-    return <div>{x}</div>
+class App2 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      number1: 0,
+      number2: 0,
+      res: 0
+    };
+    this.numberInput1Change = this.numberInput1Change.bind(this);
+    this.numberInput2Change = this.numberInput2Change.bind(this);
+  }
+  
+  numberInput1Change(event) {
+    const first = Number(event.target.value);
+    this.setState({
+      number1: first,
+      res: first + this.state.number2
+    })
+  }
+  numberInput2Change(event) {
+    const second = Number(event.target.value);
+    this.setState({
+      number2: second,
+      res: second + this.state.number1
+    })
+  }
+  
+  render() {
+    return (
+      <div>
+        <NumberInput handleNumberInput1Change={this.numberInput1Change} handleNumberInput2Change={this.numberInput2Change} />
+        <Sum res={this.state.res} />
+      </div>
+    )
   }
 }
 
-let number = document.getElementById('number');
-let number2 = document.getElementById('number2');
-
-function Add (event){
-  ReactDOM.render(
-  <Sum item = {number + number2} />,
-    document.getElementById('sum')
-  )
+class NumberInput extends React.Component {
+  render() {
+    return (
+      <div>
+        <input type="text" placeholder="write a number" onChange={this.props.handleNumberInput1Change} />
+        <input type="text" placeholder="write a number" onChange={this.props.handleNumberInput2Change} />
+      </div>
+    )
+  }
 }
 
-number.addEventListener('keyup', Add);
-number2.addEventListener('keyup', Add);
+class Sum extends React.Component {
+  render() {
+    return (
+      <p className="total sum">{this.props.res}</p>
+    )
+  }
+}
 
-class Mybuttons extends React.Component{
+class App3 extends React.Component{
   constructor(props){
     super(props)
     this.state = {
@@ -45,8 +90,8 @@ class Mybuttons extends React.Component{
   }
   Active(event){
     let clicked = event.target.id;
-    document.getElementById('Första_knappen').style.color = 'black';
-    document.getElementById('Andra_knappen').style.color = 'black';
+    document.getElementById('Första knappen').style.color = 'black';
+    document.getElementById('Andra knappen').style.color = 'black';
 
     document.getElementById(clicked).style.color = 'red';
     this.setState({
@@ -59,16 +104,20 @@ class Mybuttons extends React.Component{
     return(
       <div>
        <h1>{this.state.currentlyActive}</h1>
-        <button id="Första_knappen" onClick={this.Active.bind(this)}>React</button>
-        <button id="Andra_knappen" onClick={this.Active.bind(this)}>React2</button>
+        <button id="Första knappen" onClick={this.Active.bind(this)}>React</button>
+        <button id="Andra knappen" onClick={this.Active.bind(this)}>React2</button>
       </div>
     );
   }
 }
 
 
-ReactDOM.render(<Mybuttons/>, document.getElementById("part3"))
-
-
-
+ReactDOM.render(
+  (<div>
+     <App />
+      <App2 />
+        <App3 />
+  </div>),
+  document.getElementById('app')
+);
 
